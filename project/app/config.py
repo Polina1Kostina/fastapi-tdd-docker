56de52e1e00cbdb40ab46a166ed0f370789d179d
unknown <kostina.polly@gmail.com>
2023-01-17 09:57:42 +0300

    config, test, models

--- project/app/config.py
+++ project/app/config.py
@@ -1,17 +1,19 @@
 import logging
 from functools import lru_cache
-from pydantic import BaseSettings
+from pydantic import BaseSettings, AnyUrl
+import os
 
 
 log = logging.getLogger('uvicorn')
 
 
 class Settings(BaseSettings):
-    environment: str = 'dev'
-    testing: bool = 0
+    environment: str = os.getenv('ENVIRONMENT', 'dev')
+    testing: bool = os.getenv('TESTING', 0)
+    database_url: AnyUrl = os.environ.get('DATABASE_URL')
 
 
 @lru_cache()
 def get_settings() -> BaseSettings:
-    log.error('Loading config settings from the environment...')
+    log.info('Loading config settings from the environment...')
     return Settings()
